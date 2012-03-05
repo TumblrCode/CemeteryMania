9b3f0faf432e829f270f834963e3398eadaec946
Ryan Doenges <rhdoenges@gmail.com>
2012-03-04 19:49:40 -0800

    better resource loader

---
+++ resource.js
@@ -0,0 +1,17 @@
+resources =
+{
+	GRASS : "grass_128x64.png",
+	WATER : "water_128x64.png",
+	TOMBSTONE : "squaretombstone_128x128.png",
+	NONE : null
+}
+
+for (name in resources) {
+	var img = new Image();
+	if (resources[name] == null) {
+		img.src = null;
+	} else {
+		img.src = "assets/" + resources[name];
+	}
+	resources[name] = img;
+}

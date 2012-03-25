db11c97fc0ef66a99af5579036328ce8bd524497
mgoeppner <mgoeppner@gmail.com>
2012-03-25 03:03:18 -0400

    Preliminary facebook integration, as well as basic redis integration

--- app.js
+++ app.js
@@ -1,22 +1,69 @@
 
 /**
  * Module dependencies.
+ * 
+ * If Redis server is hosted elsewhere update createClient accordingly!
+ *
  */
 
-var express = require('express');
-var map = require('map');
-
+var express = require('express'), redis = require('redis'), redis_client = redis.createClient(), map = require('map'), facebookRequest = require('facebook-signed-request'), httprequest = require('request');
 var app = module.exports = express.createServer(), io = require('socket.io').listen(app);
 
 
-function testData()
-{
-	var x = Math.floor(Math.random()*11);
-	var y = Math.floor(Math.random()*11);
-	
-	return { X: x, Y: y };
-}
+// Redis
+//redis_client.auth("PASSWORD");
 
+redis_client.on("error", function(err)
+{
+	console.log("RedisError: " + err);
+});
+
+
+// Express Configuration
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: "CM Secret!" }));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+// Facebook config
+var fb_appID = "181403871903302";
+var fb_secret = "CENSORED";
+
+facebookRequest.secret = fb_secret;
+
+app.post('/', function(req, res)
+{
+	// Handle inital FB post request
+	var signedRequest = new facebookRequest(req.body.signed_request);
+	signedRequest.parse(function(error, request)
+	{
+		if(request.isValid())
+		{
+			if(typeof request.data.oauth_token === 'string')
+			{
+				// TODO: rewrite this, but I am lazy right now
+				var user_data;
+				httprequest("https://graph.facebook.com/" + request.data.user_id + "?access_token=" + request.data.oauth_token, function(err, response, body)
+					{ 
+						user_data = JSON.parse(body); 
+						redis_client.set("users." + request.data.user_id + ".name", user_data.first_name + " " + user_data.last_name); 
+					});
+				res.redirect('/');
+			}
+			else
+				res.render('auth.jade');
+		}
+		else
+			console.log(error);
+	});
+	
+		
+});
 
 // WebSockets
 
@@ -36,17 +83,28 @@ var chat = io.of('/chat').on('connection', function(socket)
 	});	
 });
 
-
-
-
-// Express Configuration
-
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+// Game Server
+var game = io.of('/game').on('connection', function(socket)
+{
+	
+	socket.on('client-request-player-data', function(data)
+	{
+		var authenticated = false;
+		// Authenticate player
+					
+		var playerData = {};	
+		
+		if(authenticated)
+			socket.emit('game-player-data', playerData);			
+		else
+			socket.emit('game-player-data', { exception : "AuthError"});		
+	});
+	
+	// Handle game events here
 });
+
+// End WebSockets
+
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -58,6 +116,8 @@ app.configure('production', function(){
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// Map data
 
 map.LoadMap("map/map.cmm", function(MapPacket)
 {
